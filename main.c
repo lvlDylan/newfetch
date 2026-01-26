748ca53c927f729910b79c761d33d4721fb7821f
Dylan Lavieille <55927823+lvlDylan@users.noreply.github.com>
2026-01-26 23:45:27 +0100

    Adding main.c to commit :)

--- main.c
+++ main.c
@@ -6,6 +6,58 @@
 #include <stdlib.h>
 
 #include "system.h"
+#include "colors.h"
+
+void display_fetch(sys_config_t *config) {
+
+    const char *arch_logo[] = {
+        "                   -                ",
+        "                  .o+`               ",
+        "                 `ooo/               ",
+        "                `+oooo:              ",
+        "               `+oooooo:             ",
+        "               -+oooooo+:            ",
+        "             `/:-:++oooo+:           ",
+        "            `/++++/+++++++:          ",
+        "           `/++++++++++++++:         ",
+        "          `/+++ooooooooooooo/`       ",
+        "         ./ooosssso++osssssso+`      ",
+        "        .oossssso-````/ossssss+`     ",
+        "       -osssssso.      :ssssssso.    ",
+        "       :osssssss/        osssso+++.   ",
+        "     /ossssssss/        +ssssooo/-   ",
+        "   `/ossssso+/:-        -:/+osssso+- ",
+        "  `+sso+:-`                `.-/+oso: ",
+        " `++:.                           `-/+/",
+        " .`                                 `%"
+    };
+
+    for (int i = 0; i<19; i++) {
+        switch (i) {
+            case 2:
+                printf(CYAN BOLD "%-35s" COLOR_RESET BOLD "---------------" COLOR_RESET "\n", arch_logo[i]);
+                break;
+            case 3:
+                printf(CYAN "%-35s" "OS: " COLOR_RESET BOLD "%s" COLOR_RESET "\n", arch_logo[i], config->os_name);
+                break;
+            case 4:
+                printf(CYAN "%-35s" "Kernel: " COLOR_RESET BOLD "%s" COLOR_RESET "\n", arch_logo[i], config->kernel_name);
+                break;
+            case 5:
+                printf(CYAN "%-35s" "Uptime: " COLOR_RESET BOLD "%s" COLOR_RESET "\n", arch_logo[i], config->uptime);
+                break;
+            case 6:
+                printf(CYAN "%-35s" "Memory: " COLOR_RESET BOLD "%s/%s" COLOR_RESET "\n", arch_logo[i], config->free_ram, config->total_ram);
+                break;
+            case 7:
+                printf(CYAN "%-35s" "CPU: " COLOR_RESET BOLD "%s" COLOR_RESET "\n", arch_logo[i], config->cpu_name);
+                break;
+            default:
+                printf(CYAN "%-35s" COLOR_RESET "\n", arch_logo[i]);
+                break;
+        }
+    }
+};
 
 int main(void)
 {
@@ -18,7 +70,8 @@ int main(void)
     get_ram_info(config);
     get_os_name(config);
     get_kernel_version(config);
-    printf("%s - \nMemory: %s / %s\nSystem Name: %s\nKernel: %s", config->cpu_name, config->free_ram, config->total_ram, config->os_name, config->kernel_name);
+    get_uptime(config);
 
+    display_fetch(config);
     free(config);
 }
